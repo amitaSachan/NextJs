a95cf8bfd40b50ade852b9cfa7674873525034ca
amitasachan <sachan.amita33@gmail.com>
2024-02-28 13:26:58 +0530

    navigation with useRouter hook using function

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -1,9 +1,17 @@
-const aboutUs=()=>{
-    return(
-        <>
-        <h1>About Us Page</h1>
-        </>
-    )
-}
+"use client";
+import { useRouter } from "next/navigation";
+
+const aboutUs = () => {
+  const router = useRouter();
+  const navigate = (name) => {
+    router.push(name);
+  };
+  return (
+    <>
+      <h1>About Us Page</h1>
+      <button onClick={() => navigate("/")}>Go to Home Page</button>
+    </>
+  );
+};
 
 export default aboutUs;
