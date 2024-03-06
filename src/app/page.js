417f701cdeb73afc75a9eaad749025ee6dff9c6f
amitasachan <sachan.amita33@gmail.com>
2024-03-06 13:14:58 +0530

    added generatemetadata function

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,19 @@
-import profileImg from "../../public/vercel.svg";
-import Image from "next/image";
 export default function Home() {
-  console.log(profileImg);
+
   return (
     <main>
-      <h2>
-        Image Optimization-optimize the image having width, height and src props
-        also lazy loads the image
-      </h2>
-      <Image src={profileImg} />
-      <h1> we can use Html img tag but its not optimize the image </h1>
-      <img src={profileImg.src} width={300} heigth={300} />
-      <h1>
-        {" "}
-        need to register image domain if we are using any online image in
-        next.config file and width and height property are must{" "}
-      </h1>
-      <Image
-        src="https://www.shutterstock.com/image-photo/red-apple-isolated-on-white-600nw-1727544364.jpg"
-        width={300}
-        height={300}
-      />
+    
+      <h2> generate meta data we are using when we want more dynamic metadata like title and dexcription in seo. We can also use this when, we are creating meta data dynamically  </h2>
+   
     </main>
   );
+}
+export function generateMetadata()
+{
+  return(
+    {
+      title:"For ecommerce",
+      description:"any imortabat nfjjidfjdf"
+    }
+  )
 }
